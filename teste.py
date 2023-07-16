691bdbd326954a2e528296bd5a24560506f9b8c3
Luiz Vinícius <viniciusadr244@gmail.com>
2023-07-16 12:02:03 -0300

    What I have to do

---
+++ teste.py
@@ -0,0 +1,29 @@
+# Arquivo para testar velocidade do código
+'''
+    Falta fazer:
+    1- Criar um painel de opções
+    2- Criar testes
+    3- arrumar modelo relacional (data, telefone, veículo com observação)
+
+    Painel de opções:
+    [  ] Cadastrar apartamento
+    [  ] Editar proprietário do apartamento
+    [  ] Editar moradores do apartamento (cadastrar novamente)
+    [  ] Editar cor/observação do veículo
+    [  ] Deletar morador do apartamento
+    [  ] Deletar veículo do apartamento
+    [  ] Consultas
+        [  ] Consultar apartamento (mostrar tudo)
+        [  ] Consultar proprietário (CPF ou nome)
+        [  ] Consultar morador (nº apartamento ou Nome)
+        [  ] Consultar veículo (Placa ou modelo)
+'''
+# import timeit
+
+# nome = input("nome: ")
+# start1 = timeit.default_timer()
+# obj1 = {
+#     "name": nome
+# }
+# end1 = timeit.default_timer()
+# t1 = end1 - start1

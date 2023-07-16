b6c70da0f040adaa1baf8bb613321cf7294c36d7
Luiz Vinícius <viniciusadr244@gmail.com>
2023-07-15 22:03:01 -0300

    trabalhando para reduzir acoplamento

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 '''Módulo com funções muito usadas no projeto'''
 import dotenv
+import datetime
 import vehicle
+import person
 from utils import uteis, validation
 
 
@@ -16,27 +18,33 @@ def main():
     # print("[ 4 ] Adicionar observação ao carro")
     # print("[ 5 ] Editar cor do carro(s)")
     # apartament = apartament_register()
-    # owner = person_register(owner=True)
+    # owner = person.person_register(owner=True)
 
     # msg = "Quantas pessoas vão morar no apartamento? "
     # people_in_apartament = uteis.ler_option(msg, max_opt=4, exept_msg="Quantidade inválida. Máximo 4")
     # people = {}
     # for i in range(1, people_in_apartament+1):
     #     print(f"\n{i}º Morador")
-    #     people.setdefault(f"person{i}", person_register())
+    #     people.setdefault(f"person{i}", person.person_register())
 
-    print("Você possui veículo?\n[ 1 ] Sim\n[ 2 ] Não")
-    opt = uteis.ler_option("Digite o número: ", max_opt=2)
-
-    automobile = vehicle.register(opt)
-    if opt == 1: # provisório
-        print(f"Qual alteração você quer fazer no(a) {automobile['categoria']}?\n[ 1 ] Adicionar observação\n[ 2 ] Alterar cor")
-        modification = uteis.ler_option("Digite o número: ", max_opt=2)
-        automobile = vehicle.alter(automobile, modification)
-        print(automobile)
-        automobile = vehicle.delete()
+    # print("Você possui veículo?\n[ 1 ] Sim\n[ 2 ] Não")
+    # opt = uteis.ler_option("Digite o número: ", max_opt=2)
     
-    print(automobile)
+    # if opt == 1:
+    #     print("\nQual categoria?\n[ 1 ] Carro\n[ 2 ] Moto")
+    #     category = uteis.ler_option("Digite o número: ", max_opt=2, exept_msg="Categoria inválida.")
+    #     automobile = vehicle.register(opt, category)
+    # else:
+    #     automobile = vehicle.register(opt)
+
+    # if opt == 1: # provisório
+    #     print(f"Qual alteração você quer fazer no(a) {automobile['categoria']}?\n[ 1 ] Adicionar observação\n[ 2 ] Alterar cor")
+    #     modification = uteis.ler_option("Digite o número: ", max_opt=2)
+    #     automobile = vehicle.alter(automobile, modification)
+    #     print(automobile)
+    #     automobile = vehicle.delete()
+    
+    # print(automobile)
     # print(owner)
     # print(people)
     # print(apartament)
@@ -55,30 +63,6 @@ def apartament_register():
             break
 
     return {"bloco": bloco, "apartamento": num_apto, "Banco de dados": bloco + num_apto}
-
-
-def person_register(owner=False):
-    '''Função que registra as pessoas ou proprietário que moram no apartamento.'''
-    msg = "Nome do proprietário do apartamento: " if owner else "Nome: "
-    name = "1"
-    while True:
-        name = input(msg).strip().title().split(" ")
-        if validation.name(name) is True:
-            name = " ".join(name)
-            break
-        print("Formato de nome inválido.\n")
-
-    cpf = 0
-    while True:
-        try:
-            cpf = input("CPF (no formato xxx.xxx.xxx-xx): ").strip()
-            if validation.cpf(cpf) is True:
-                break
-            raise ValueError("\033[31mFormato inválido.\033[m\n")
-        except ValueError as expt:
-            print(expt)
-
-    return {"name": name, "cpf": cpf}
 
 
 main()

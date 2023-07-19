145f5012d2d58fc114bc049bffc22e85392d9caf
Luiz Vinícius <viniciusadr244@gmail.com>
2023-07-19 11:09:44 -0300

    refactor to use class

--- vehicle.py
+++ vehicle.py
@@ -1,45 +1,41 @@
-'''Gravar a data do registro'''
-from datetime import date as d
-from utils import uteis, validation
+class Vehicle:
+    def __init__(self, placa, categoria, cor, modelo, observacao, data_cadastro):
+        self.placa = placa
+        self.categoria = categoria
+        self.cor = cor
+        self.modelo = modelo
+        self.observacao = observacao
+        self.data_cadastro = data_cadastro
 
+    def to_string(self):
+        placa = self.placa
+        cat = self.categoria
+        cor = self.cor
+        modelo = self.modelo
+        obs = self.observacao
+        data = self.data_cadastro
+        return f"Veículo: [placa: {placa}, cat: {cat}, cor: {cor}, modelo: {modelo}, obs: {obs}, data: {data}]"
 
-def register(opt, category=None):
-    '''Função que registra o veículo do apartamento.\n
-        Parâmetro opt = 1 para possui veículo, qualquer outro número para não possui.
-    '''
-    placa = color = model = date = None
-    if opt == 1:
-        category = "carro" if category == 1 else "moto"
-        
-        while True:
-            placa = input(f"Qual a placa do(a) {category}: ").strip()
-            if validation.placa(placa) is True:
-                break
-            print("Placa inválida.\n")
+    def get_placa(self):
+        return self.placa
 
-        color = uteis.ler_string(f"Qual a cor do(a) {category}: ")
-        model = uteis.ler_string(f"Qual o modelo do(a) {category}: ")
-        date = d.today()
-    car = {
-        "categoria": category,
-        "placa": placa,
-        "cor": color,
-        "modelo": model,
-        "data": date,
-        "observação": "Nenhuma observação"
-    }
-    return car
+    def get_categoria(self):
+        return self.categoria
 
+    def get_cor(self):
+        return self.cor
 
-def alter(vehicle, option):
-    '''Recebe um objeto carro e um parâmetro (1- observation; 2- color) e o retorna alterado.'''
-    vehicle_key = "observação" if option == 1 else "cor"
-    new_info = input(f"Qual a nova {vehicle_key} do(a) {vehicle['categoria']}: ").strip()
+    def set_cor(self, cor):
+        self.cor = cor
 
-    vehicle[vehicle_key] = new_info
-    return vehicle
+    def get_modelo(self):
+        return self.modelo
 
+    def get_observacao(self):
+        return self.observacao
 
-def delete():
-    '''Define todas as informações do carro para None.'''
-    return {"categoria": None, "placa": None, "cor": None, "modelo": None, "data": None, "observação": None}
+    def set_observacao(self, observacao):
+        self.observacao = observacao
+
+    def get_data(self):
+        return self.data_cadastro

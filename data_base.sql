b263bacbf463335b6dc5ed26098cddf55f5c006d
Luiz Vinícius <viniciusadr244@gmail.com>
2023-07-21 11:40:41 -0300

    tables ok

---
+++ data_base.sql
@@ -0,0 +1,39 @@
+-- create database if not exists apartament (não existe)
+-- \c apartament
+create table if not exists Proprietario(
+	cpf varchar(11),
+	nome varchar(50) not null,
+	telefone varchar(11),
+	data_cadastro date,
+	primary key (cpf)
+);
+
+create table if not exists Apartamento(
+	numero varchar(5),
+	data_cadastro date,
+	cpf varchar(11),
+	primary key (numero),
+	foreign key (cpf) references Proprietario(cpf)
+);
+
+create table if not exists Pessoa(
+	cpf varchar(11),
+	nome varchar(50) not null,
+	telefone varchar(11) not null,
+	data_cadastro date,
+	num_apto varchar(5),
+	primary key (cpf),
+	foreign key (num_apto) references Apartamento(numero)
+);
+
+create table if not exists Veiculo(
+	placa varchar(7),
+	categoria varchar(5) not null,
+	cor varchar(20),
+	modelo varchar(20) not null,
+	observacao varchar(50),
+	num_apto varchar(5),
+	data_cadastro date,
+	primary key (placa),
+	foreign key (num_apto) references Apartamento(numero)
+)

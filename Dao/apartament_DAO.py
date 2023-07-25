b6943e237318a7ee66df23e8c9164f0da939f247
Luiz Vinícius <viniciusadr244@gmail.com>
2023-07-25 18:08:08 -0300

    DAO ok

--- Dao/apartament_DAO.py
+++ Dao/apartament_DAO.py
@@ -1,10 +1,20 @@
+import psycopg2
 from pool_cursors import Cursors
 from apartament import Apartament
 
 
 class Apartament_DAO:
-    def insert(self):
-        sql = "insert into apartamento () values ()"
+    def insert(self, apt: Apartament, cpf):
+        sql = "insert into apartamento (numero, cpf, data_cadastro) values (%s, %s, %s)"
+        try:
+            with Cursors().create_cursor() as cursor:
+                cursor.execute(sql, (apt.get_numero(), cpf, apt.get_data()))
+                print("Apartamento criado.")
+        except psycopg2.Error as erro:
+            print(erro)
 
-    def delete(self):
-        sql = "delete from apartamento where numero = "
+        Cursors.close_cursor()
+
+    def select(self):
+        
+        Cursors.close_cursor()
--- Dao/person_DAO.py
+++ Dao/person_DAO.py
@@ -1,3 +1,4 @@
+import psycopg2
 from pool_cursors import Cursors
 from person import Person
 
@@ -7,21 +8,32 @@ class Person_Dao:
         sql = "insert into pessoa (cpf, nome, telefone, data_cadastro, num_apto) values (%s, %s, %s, %s, %s);"
         values = (person.get_cpf(), person.get_nome(), person.get_telefone(), person.get_data_cadastro(), num_apto)
 
-        with Cursors.create_cursor() as cursor:
-            cursor.execute(sql, values)
-            Cursors.close_cursor()
-        print(f"{person.get_nome()} adicionado(a).")
+        try:
+            with Cursors.create_cursor() as cursor:
+                cursor.execute(sql, values)
+            print(f"{person.get_nome()} adicionado(a).")
+        except psycopg2.Error as erro:
+            print(erro)
+        Cursors.close_cursor()
 
     def delete(self, cpf):
-        with Cursors.create_cursor() as cursor:
-            cursor.execute("delete from pessoa where cpf = %s", (cpf,))
-            Cursors.close_cursor()
-        print("Morador excluído.")
+        try:
+            with Cursors.create_cursor() as cursor:
+                cursor.execute("delete from pessoa where cpf = %s", (cpf,))
+            print("Morador excluído.")
+        except psycopg2.Error as erro:
+            print(erro)
+
+        Cursors.close_cursor()
 
     def select(self, num_apto):
-        with Cursors.create_cursor() as cursor:
-            cursor.execute("select * from pessoa where num_apto = %s", (num_apto,))
-            res = cursor.fetchall()
-            Cursors.close_cursor()
-        print("Select realizado.")
+        try:
+            with Cursors.create_cursor() as cursor:
+                cursor.execute("select * from pessoa where num_apto = %s", (num_apto,))
+                res = cursor.fetchall()
+            print("Select realizado.")
+        except psycopg2.Error as erro:
+            print(erro)
+
+        Cursors.close_cursor()
         return res

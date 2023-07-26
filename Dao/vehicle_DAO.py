9e55e148ffb995cfab24c5f6100aaf2c626b7f65
Luiz Vinícius <viniciusadr244@gmail.com>
2023-07-26 08:23:23 -0300

    add select

--- Dao/vehicle_DAO.py
+++ Dao/vehicle_DAO.py
@@ -5,7 +5,7 @@ from vehicle import Vehicle
 
 class Vehicle_DAO:
     def insert(self, vehicle: Vehicle, num_apto):
-        sql = "insert into veiculo (placa, categoria, cor, modelo, observacao, data_cadastro, num_apto) values (%s, %s, %s, %s, %s, %s, %s);"
+        sql = "insert into veiculo (placa, categoria, cor, modelo, observacao, data_cadastro, num_apto) values (%s, %s, %s, %s, %s, %s, %s)"
         values = (vehicle.get_placa(), vehicle.get_categoria(), vehicle.get_cor(), vehicle.get_modelo(), vehicle.get_observacao(), vehicle.get_data(), num_apto)
 
         try:
@@ -28,10 +28,25 @@ class Vehicle_DAO:
         Cursors.close_cursor()
 
     def update(self, field, new_value, placa):
+        sql = "update veiculo set (%s) = (%s) where placa = (%s)"
         try:
             with Cursors.create_cursor() as cursor:
-                cursor.execute("update veiculo set (%s) = (%s) where placa = (%s)", (field, new_value, placa))
+                cursor.execute(sql, (field, new_value, placa))
             print(f"{field} alterado(a).")
         except psycopg2.Error as erro:
             print(erro)
+
         Cursors.close_cursor()
+
+    def select(self, placa):
+        sql = "select * from veiculo where placa = (%s)"
+        try:
+            with Cursors.create_cursor() as cursor:
+                cursor.execute(sql, (placa,))
+                res = cursor.fetchall()
+            print("Select realizado.")
+        except psycopg2.Error as erro:
+            print(erro)
+
+        Cursors.close_cursor()
+        return res

749b937be38f2518cc5ea3d5f2ae6879ae7ec385
Luiz Vinícius <viniciusadr244@gmail.com>
2023-07-27 22:24:43 -0300

    new methods

--- utils/formatting.py
+++ utils/formatting.py
@@ -11,7 +11,46 @@ def phone_number(number):
     return f"({number[0:2]}) {number[2:7]}-{number[7:]}"
 
 
+def split_num_apt(num):
+    return f"Bloco {num[0:2]} Número {2:}"
+
+
+def date(str_date):
+    return str_date.strftime("%d/%m/%Y")
+
+
 def show_array(array):
     '''Função que mostra um objeto na forma listada.'''
     for i, msg in enumerate(array, start=1):
         print(f"[ {i} ] {msg}")
+
+
+def show_apartament(obj):
+    num_apt = split_num_apt(obj['apartamento'][0][0])
+    print(f"\nProprietário do apartamento {num_apt}.")
+    show_person(obj['proprietario'])
+
+    print(f"\nMoradores do apartamento {num_apt}.")
+    show_person(obj['moradores'])
+
+    if len(obj['veiculo']) != 0:
+        show_vehicle(obj['veiculo'])
+
+
+def show_person(obj):
+    for i in obj:
+        print(f"\t{i[1]}")
+        print(f"\tCPF: {cpf(i[0])}")
+        print(f"\tTelefone: {phone_number(i[2])}")
+        print(f"\tData de cadastro: {date(i[3])}\n")
+
+
+def show_vehicle(obj):
+    print("\nVeículos do apartamento.")
+    for i in obj:
+        print(f"\tPlaca: {i[0]}")
+        print(f"\tCategoria: {i[1]}")
+        print(f"\tCor: {i[2]}")
+        print(f"\tModelo: {i[3]}")
+        print(f"\tData de cadastro: {date(i[6])}")
+        print(f"\tObservação: {i[4]}\n")
